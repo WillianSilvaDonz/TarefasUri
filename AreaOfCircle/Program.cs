57d8cf54485efc58e1742441c858b3825842d09b
Willian <williansilva@unochapeco.edu.br>
2020-08-11 08:46:01 -0300

    first commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            valor = Convert.ToDouble(Console.ReadLine().Replace(".",","));
+            double result = Math.PI * (Math.Pow(2, valor));
+            Console.WriteLine($"A={string.Format("{0:0.0000}", result)}");
+        }
+    }
+}
---
+++ ExtremelyBasic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExtremelyBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int entrada, entrada2;
+            entrada = Convert.ToInt32(Console.ReadLine());
+            entrada2 = Convert.ToInt32(Console.ReadLine());
+            int result = entrada + entrada2;
+            Console.WriteLine("X = "+ result);
+        }
+    }
+}
